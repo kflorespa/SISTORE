bb07eef5a3c50238039bfb8134b106b273df301f
Kevin Flores <niickcreew@gmail.com>
2019-04-23 23:03:56 -0500

    Sentencias
    sql

--- Code/SISTORE/src/AD/conexionMS.java
+++ Code/SISTORE/src/AD/conexionMS.java
@@ -1,11 +1,7 @@
 package AD;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import javax.swing.JOptionPane;
 
 public class conexionMS {
     public String db="FINADB";
@@ -16,24 +12,10 @@ public class conexionMS {
     Statement sn;
     
     public Connection conexion() {
-        
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            cn = DriverManager.getConnection(this.url, this.user, this.pass);
-        } catch (ClassNotFoundException | SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error de autenticación database");
-        }
         return cn;
     }
     
     public Statement sentencia(){
-        try {
-            sn=conexion().createStatement(
-                    ResultSet.TYPE_SCROLL_SENSITIVE,
-                    ResultSet.CONCUR_READ_ONLY);
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, " No hay conexión");
-        }
         return sn;
     }
     
